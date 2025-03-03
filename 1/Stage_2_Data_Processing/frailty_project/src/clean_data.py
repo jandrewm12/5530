a98717b242c3a11967f94316272ababc0aaaf268
Jacob McIntosh <60158099+jandrewm12@users.noreply.github.com>
2025-03-02 22:45:06 -0600

    Delete 1 directory

--- 1/Stage_2_Data_Processing/frailty_project/src/clean_data.py
+++
@@ -1,7 +0,0 @@
-# Stage Two of Reproducible Workflow
-
-# In stage two, we focus on processing the raw data. For the frailty data set, there does not appear to be any null cells to remove / replace.
-# None of the participant's data appears to be an outlier either. There is a wide range of values for each physical attribute, but none are beyond what would be expected. 
-# Because of these factors, this particualr dataset does not need any cleanup. 
-# However, we could write a script that would go through and clean up for any null values or outliers if we were expecting the dataset to be expanded upon or to make the script useful for additional datasets in the future.
-
