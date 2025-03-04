ce899abc82d726c41ae010316c6af124cf0cd35e
Jacob McIntosh <60158099+jandrewm12@users.noreply.github.com>
2025-03-03 21:53:38 -0600

    Add files via upload

---
+++ Assignment01/1/Stage_3_Data_Analysis/frailty_project/src/analysis.py
@@ -0,0 +1,9 @@
+# Stage 3: Data Analysis
+
+
+# In stage 3 of reproducible workflow, we focus on analyzing the cleaned dataset. 
+# For the frailty dataset, there are several approaches to draw out conclusion based on the data.
+# One such analysis would be to separate those with and without frailty into two separate groups. Then, calculate the averages of each physical attribute for both groups.
+# Once the average physical values are calculated for both groups, compare and see if there are any major differences between them. For example, are the members of the frailty group significantly short than the non-frailty group? Is their grip strength significantly lower?
+# If there isn't a particularly large difference between the two groups, you could check and see if there exists a combination of factors that appear to influence the presence of frailty. "Does a low grip strength and low weight lead to frailty" is an example of such a comparison.
+# Once the analysis has concluded, a txt document should be generated with a report of the program's findings.
---
+++ Assignment01/2/student_performance/data_raw/README.txt
@@ -0,0 +1,14 @@
+
+Assignment 01
+
+Question 02
+
+Data was downloaded from the provided box link and placed in the data_raw folder.
+
+All the code for this question is contained in the notebook in the src folder.
+
+StudentsPerformance.csv does not contain any null data cells or any other oddities needing cleaning, so I moved straight to analysis.
+
+All visualizations are contained in the notebook as well as the results folder.
+
+Graphs were saved to results folder using a path link to my Google Drive. For testing, please update the path to the proper location in your drive.
